0167717cc01b04c7011f9fe0fe9efb4351b27dbd
Vismay Malviya <90767593+vismaymalviya@users.noreply.github.com>
2024-03-20 04:02:46 +0000

    datatypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as never version
+
+// alert(3 + 3) // we are using node js and not a browser hence it will give error
+
+console.log(3 + 3); 
+// code readability should always be high
+console.log("Vismay")
+
+let name = "Vismay"  // string datatype
+let age = 22 // number value
+let isLoggedIn = false // boolean
+let state; // undefined
+let temp = null // we defined that temp is empty
+
+// PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigint => very large number
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => not defined
+// symbol => defines individual components/ uniqueness
+
+// object
+
+console.log(typeof age)
+;console.log(typeof undefined); // undefined <-output
+console.log(typeof null); // object <-output
